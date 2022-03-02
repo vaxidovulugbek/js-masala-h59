d8dae82287c79db2c2c160fb50d8687cd72819e5
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-03-02 09:01:41 +0500

    comit

---
+++ js/main.js
@@ -0,0 +1,184 @@
+// 2 masala
+
+// let a = +prompt("a son ")
+// let b = +prompt("b son ")
+
+// let c = (((1 / 6) * Math.sqrt(a)) + ((1 / 3) * Math.sqrt(b))) ** (2)
+// console.log(c)
+
+
+
+// 3 masala togri
+// let a = '123'
+// let b =Math.floor( a/100%10 )
+// let c =Math.floor( a /10%10 )
+// let d =Math.floor( a%10 )
+// console.log(d+''+c+ '' +b)
+
+// let son = prompt("a ")
+// let a = son % 10;
+// let b = (son % 10) % 10
+// let d = son / 100 
+// console.log(a,b,d)
+
+
+// 4 masala togri
+// let a = 454
+// let b =Math.floor( a/100%10 )
+// let c =Math.floor( a /10%10 )
+// console.log(c+''+b+'' +d)
+
+
+
+
+// 5 - masala
+// let a = +prompt("a: ")
+// let b = +prompt("bni qiymati: ")
+
+// let c = (9 * (a ** 2)) - (27 * (a ** 2) * (b ** 2)) + (15 * (b ** 2))
+// console.log(c)
+
+
+
+
+
+// 6 - masala
+// let r = +prompt("r soni")
+// let n = +prompt("n soni")
+// let a = +prompt("a soni")
+// let b = +prompt("b soni")
+
+// let c = ( (1 + (r / 100) ** n) / (Math.sqrt(a ** 2) + (b ** 2)) )
+// console.log(c)
+
+
+
+//  7 masala
+// let x1 = -prompt("x1 soni")
+// let x2 = +prompt("x2 soni")
+// let y1 = +prompt("y1 soni")
+// let y2 = +prompt("y2 soni")
+
+// let c = (Math.sqrt((x2 - x1) ** (2)) + (y2 + y1) ** (2))
+// console.log(c)
+
+
+//8 masala 
+// let x1 = +prompt("x1: ")
+// let x2 = +prompt("x2: ")
+// let y1 = +prompt("y1: ")
+// let y2 = +prompt("y2: ")
+// let x3 = +prompt("x3: ")
+// let y3 = +prompt("y3: ")
+
+
+
+
+
+
+// masala 11 togri
+// let a = +prompt("a: ")
+// let b = +prompt("b: ")
+// console.log( a == b)
+
+
+
+
+// 12 masala togri
+// let a = +prompt("a:")
+// console.log(a == 7 )
+
+
+
+// 13 masala togri
+// let b = +prompt("b :")
+// console.log(b > 0)
+
+
+// 14 masala togri
+// let a = +prompt("a :")
+// console.log(a < 0)
+
+
+// 15 masala togri
+// let a = +prompt("a :")
+// console.log(a % 2 == 1)
+
+
+// 16 masala togri
+// let b = +prompt("b :")
+// console.log(b % 2 == 0)
+
+
+// 17 masala togri
+// let a = +prompt("a: ")
+// let b = prompt("b :")
+// let c = +prompt("c :")
+// console.log(a < 100 || a < c)
+
+
+// 18 masaal togri
+// let a = Number(prompt("a:"))
+// let b = Number(prompt("b:"))
+// console.log(a >= 0 && b >= 0)
+
+
+// 19 masaal togri
+// let a = Number(prompt("a:"))
+// let b = Number(prompt("b:"))
+// console.log(a <! 0 && b <! 0)
+
+
+// 20 masaal togri
+// let a = Number(prompt("a:"))
+// let b = Number(prompt("b:"))
+// console.log(a % 2 == 0 && b % 2 == 0)
+
+
+
+// 22 masala togri 
+// let n = Number(prompt("n:"))
+// console.log(2 * (Math.pow(n + 3 , 2)))
+
+
+
+// 23 masala togri 16.0 chiqishi kerak
+// let n = Number(prompt("n:"))
+// console.log(Math.pow((3 + n) / 2 , 2))
+
+
+
+// 24 misol togri
+// let n = Number(prompt("n:"))
+// let x = Number(prompt("x:"))
+// console.log(Math.pow(n , x) + Math.pow(6 , x))
+
+
+// 25 misol togri berilgan sonni manfiy bolsa ham musbat qilib beradi
+// let a = Number(prompt("a:"))
+// console.log(Math.abs(a))
+
+
+// 26 misol ishlamdi koish kerak
+// let m = Number(prompt("m:"))
+// console.log(Math.round(2))
+
+
+
+// 27 misol togri
+// let x = Number(prompt("x:"))
+// let y = Number(prompt("y:"))
+// console.log(Math.pow(x , 4) + (5 * Math.pow(x , 2)) + (Math.pow(x , 3) * y))
+
+
+// 28 misol togri
+// let x = Number(prompt("x:"))
+// let y = Number(prompt("y:"))
+// console.log((6 * Math.pow(x , 3) * Math.pow(y , 5)) + (4 * Math.pow(x , 4) * Math.pow(y , 3)) - (24 * x * y))
+
+
+// 30 misol togri
+// console.log(Math.abs(( ((3*1)+2) / 3) - ((4 * 3) + 1) / 4))
+
+
+
